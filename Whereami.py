ab4fdc6af9ad59a301f0344ea75caf3b2733554b
Pfau <BAL_mhpfau@seattleschools.org>
2019-03-28 15:05:40 -0700

    I did some minecraft coding

--- Whereami.py
+++ Whereami.py
@@ -1,6 +1,5 @@
 import mcpi.minecraft as minecraft
 mc = minecraft.Minecraft.create()
 pos = mc.player.getTilePos()
-print(pos.x)
-print(pos.y)
-print(pos.z)
+print("x="+str(pos.x) +" y="+str(pos.y) +" z="+str(pos.z))
+mc.postToChat("x="+str(pos.x) +" y="+str(pos.y) + " z="+str(pos.z) )
